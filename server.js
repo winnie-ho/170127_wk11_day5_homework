723f246af57a340c715ce0ccf36dc75db3d48f9f
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-02-11 08:15:19 +0000

    fixed the port issue

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
 
 app.use(express.static('public'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.port || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
