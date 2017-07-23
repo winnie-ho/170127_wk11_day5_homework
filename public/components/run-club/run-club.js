87c02617e39f6ff2bcbcba17dd169aa729f18941
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-07-23 11:16:22 +0100

    adjust for api-call abstraction for run-club component

--- public/components/run-club/run-club.js
+++ public/components/run-club/run-club.js
@@ -1,12 +1,5 @@
 var urlRuns = "https://www.strava.com/api/v3/athlete/activities?per_page=200&access_token=a2ff6fffcab9df06d90661ad34b7e664690c4fc4"
 
-var requestRunClubRunsComplete = function (){
-  if (this.status !== 200) return;
-  result = JSON.parse(this.responseText);
-  // console.log("RUN CLUB CALL",result);
-  computeRunClubRuns(result);
-}
-
 var handleRunClubButton = function() {
   var runClubDiv = document.getElementById('run-club');
   var runsDiv = document.getElementById('runs');
@@ -16,6 +9,7 @@ var handleRunClubButton = function() {
       runsDiv.style.display = 'none';
       parkRunDiv.style.display = 'none';
       runClubDiv.style.display = 'flex';
+      makeRequest(urlRuns, computeRunClubRuns);
   } else {
       runClubDiv.style.display = 'none';
   } 
