a170c7aedd8a92f92e473322e1e0c731cdc43d5b
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-08-20 10:25:25 +0100

    using activity summary in run club to render runs

--- public/components/run-club/run-club.js
+++ public/components/run-club/run-club.js
@@ -1,90 +1,11 @@
 const computeRunClubRuns = (responseRuns) => {
   const runClubRuns = responseRuns.filter(run => (55.94 < run.start_latitude < 55.95 && (run.start_longitude === -3.21 || run.start_longitude === -3.20) && (new Date (run.start_date_local)).getDay() === 1));
 
-  displayRunClubRunsDate(runClubRuns);
-  displayRunClubRunsName(runClubRuns);
-  displayRunClubRunsDistance(runClubRuns);
-  displayRunClubRunsTime(runClubRuns);
-  displayRunClubRunsPace(runClubRuns);
-  return runClubRuns;
-}
+  let runClubDiv = document.querySelector("#run-club");
+  runClubDiv.innerHTML = "";
 
-const displayRunClubRunsDate = (runClubRuns) => {
-  const runClubDiv = document.querySelector("#run-club-date");
-  for (const run of runClubRuns){
-    const date = document.createElement("div");
-    date.classList.add("data-metric", 'data-long');
-    date.innerText = run.start_date.substr(8,2) + "-" + run.start_date.substr(5,2) + "-" + run.start_date.substr(0,4);
-    runClubDiv.appendChild(date);
-  }
-}
-
-const displayRunClubRunsName = (runClubRuns) => {
-  const runClubDiv = document.querySelector("#run-club-name");
-  for (const run of runClubRuns){
-    const name = document.createElement("div");
-    name.classList.add("data-metric", 'data-long');
-    name.innerText = run.name;
-    runClubDiv.appendChild(name);
-  }
-}
-
-const displayRunClubRunsDistance = (runClubRuns) => {
-  const runClubDiv = document.querySelector('#run-club-distance');
-  for (const run of runClubRuns){
-    const distance = document.createElement("div");
-    distance.classList.add("data-metric");
-    const distanceIcon = document.createElement("img");
-    distanceIcon.src = "./resources/icon_distance.png";
-    distanceIcon.classList.add("icon-metric");
-    const distanceValue = document.createElement("div");
-    distanceValue.innerText = ((run.distance)/1000).toFixed(2) + "km";
-    distance.appendChild(distanceIcon);
-    distance.appendChild(distanceValue);
-
-    runClubDiv.appendChild(distance); 
-  }
-}
-
-
-const displayRunClubRunsTime = (runClubRuns) => {
-  const runClubDiv = document.querySelector("#run-club-time");
-  for (const run of runClubRuns){
-
-    const time = document.createElement("div");
-    time.classList.add("data-metric");
-    const timeIcon = document.createElement("img");
-    timeIcon.src = "./resources/icon_time.png";
-    timeIcon.classList.add("icon-metric");
-    const timeValue = document.createElement("div");
-
-    let rawTime = run.moving_time;
-    timeValue.innerText = renderTime(rawTime);
-    time.appendChild(timeIcon);
-    time.appendChild(timeValue);
-
-    runClubDiv.appendChild(time); 
-  }
-}
-
-const displayRunClubRunsPace = (runClubRuns) => {
-  const runClubDiv = document.getElementById("run-club-pace");
-  for (const run of runClubRuns){
-    const pace = document.createElement("div");
-    pace.classList.add("data-metric");
-
-    const paceIcon = document.createElement("img");
-    paceIcon.src = "./resources/icon_pace.png";
-    paceIcon.classList.add("icon-metric");
-
-    let rawTime = run.moving_time;
-    let rawDistance = run.distance;
-    const paceValue = document.createElement("div");
-
-    paceValue.innerText = renderPace(rawTime, rawDistance);
-    pace.appendChild(paceIcon);
-    pace.appendChild(paceValue);
-
-    runClubDiv.appendChild(pace);
-  }
+  runClubRuns.forEach(run => {
+    let activitySummary = createActivitySummary(run.start_date, run.name, run.distance, run.moving_time); 
+    runClubDiv.appendChild(activitySummary);
+  });
 }
