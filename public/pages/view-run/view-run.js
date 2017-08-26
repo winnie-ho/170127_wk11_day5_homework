9b595a27b8d339d195a4dba57570088780515e64
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-08-26 14:17:10 +0100

    heart rate and cadence added to view run

--- public/pages/view-run/view-run.js
+++ public/pages/view-run/view-run.js
@@ -10,6 +10,8 @@ const renderRunInfo = (rawRun) => {
   const timeDiv = document.querySelector("#primary-stats__time");
   const paceDiv = document.querySelector("#primary-stats__pace");
   const kudosDiv = document.querySelector("#secondary-stats__kudos");
+  const heartrateDiv = document.querySelector("#secondary-stats__heartrate");
+  const cadenceDiv = document.querySelector("#secondary-stats__cadence");
 
   dateDiv.innerHTML = renderDate(rawRun.start_date);
   titleDiv.innerHTML = rawRun.name;
@@ -18,4 +20,6 @@ const renderRunInfo = (rawRun) => {
   paceDiv.innerHTML = renderPace(rawRun.moving_time, rawRun.distance);
 
   kudosDiv.innerHTML = rawRun.kudos_count;
+  heartrateDiv.innerHTML = "♡" + rawRun.average_heartrate;
+  cadenceDiv.innerHTML = "↻" + rawRun.average_cadence;
 }
