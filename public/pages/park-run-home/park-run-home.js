38a5cb8c8788e990321f3889c26950a44f630110
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-09-17 18:37:24 +0100

    adding in year best context. layout fixes

--- public/pages/park-run-home/park-run-home.js
+++ public/pages/park-run-home/park-run-home.js
@@ -1,11 +1,16 @@
 const renderParkRunHome = (parkRuns) => {
   const lastPR = parkRuns[0];
+  const finishTimes = parkRuns.map(run => run.moving_time);
+  const orderedFinishTimes = finishTimes.sort((a, b) => a - b);
+  const yearBest = orderedFinishTimes[0];
+
   document.querySelector("#last-pr__date").innerHTML = "LAST RAN: " + renderDate(lastPR.start_date);
   document.querySelector("#last-pr__name").innerHTML = lastPR.name;
   document.querySelector("#last-pr__time").innerHTML = renderTime(lastPR.moving_time);
   document.querySelector("#last-pr__pace").innerHTML = renderPace(lastPR.moving_time, lastPR.distance);
   document.querySelector("#last-pr__context").innerHTML = computeLastPRContext(parkRuns);
   document.querySelector("#pb").innerHTML = computePBContext(lastPR);
+  document.querySelector("#year-best").innerHTML = computeYBContext(lastPR, yearBest);
 }
 
 const computeLastPRContext = (parkRuns) => {
@@ -24,4 +29,9 @@ const computePBContext = (lastPR) => {
   const pb = (22 * 60) + 54;
   if (lastPR.moving_time < pb) return "New PB! " + renderTime(lastPR.moving_time);
   return "PB remains at " + renderTime(pb);
+}
+
+const computeYBContext = (lastPR, yearBest) => {
+  if (lastPR.moving_time < yearBest) return "Fastest time this year! " + renderTime(lastPR.moving_time);
+  return "Year best remains at " + renderTime(yearBest);
 }
