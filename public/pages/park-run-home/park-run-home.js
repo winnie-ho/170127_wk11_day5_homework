37067a4ca56c90b3b6fb342275759a57bd8fbf67
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-10-21 18:55:23 +0100

    Including park runs that are incomplete

--- public/pages/park-run-home/park-run-home.js
+++ public/pages/park-run-home/park-run-home.js
@@ -1,13 +1,11 @@
 let pb = (22 * 60) + 54;
 
-
-
 const renderParkRunHome = (parkRuns) => {
 
   let clone = parkRuns.slice();
 	let distanceCheck = clone.filter(run => run.distance > 5000);
 
-  const lastPR = distanceCheck[0];
+  const lastPR = parkRuns[0];
   const finishTimes = distanceCheck.map(run => run.moving_time);
   const orderedFinishTimes = finishTimes.sort((a, b) => a - b);
   const yearBest = orderedFinishTimes[0];
@@ -23,12 +21,17 @@ const renderParkRunHome = (parkRuns) => {
 
 const computeLastPRContext = (parkRuns) => {
   const lastPR = parkRuns[0];
-  const orderedPRTimes = parkRuns.sort((a,b) => a.moving_time - b.moving_time);
-  
-  const lastPRRank = orderedPRTimes.indexOf(lastPR)+1;
-  if( lastPRRank === 1 ){
+  const orderedPRTimes = parkRuns.slice().sort((a,b) => a.moving_time - b.moving_time);
+  const lastPRRank = orderedPRTimes.indexOf(lastPR);
+
+  if (lastPRRank === 0 && lastPR.distance >= 5000) {
     return "Fastest this year!";
   }
+
+  if (lastPR.distance < 5000) {
+    return "RUN INCOMPLETE! " + renderDistance(lastPR.distance) + " of 5km in " + renderTime(lastPR.moving_time);
+  }
+  
   return ordinalSuffixOf(lastPRRank) + " fastest this year";
 }
 
@@ -41,14 +44,16 @@ const showParkRunGraphs = () => {
 }
 
 const computePBContext = (lastPR) => {
-  if (lastPR.moving_time < pb) {
+  if (lastPR.moving_time < pb && lastPR.distance >= 5000) {
     pb = lastPR.moving_time;
-    return "New PB! " + renderTime(lastPR.moving_time);
+    return "NEW PB! " + renderTime(lastPR.moving_time);
   }
   return "PB remains at " + renderTime(pb);
 }
 
 const computeYBContext = (lastPR, yearBest) => {
-  if (lastPR.moving_time < yearBest) return "Fastest time this year! " + renderTime(lastPR.moving_time);
+  if (lastPR.moving_time < yearBest && lastPR.distance >= 5000) {
+    return "Fastest time this year! " + renderTime(lastPR.moving_time);
+  }
   return "Year best " + renderTime(yearBest);
 }
