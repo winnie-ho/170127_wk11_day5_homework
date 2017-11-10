bc35be4c3a8b10d13df1329ee27695006b181585
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-11-10 22:27:11 +0000

    change page after computes

--- public/components/nav/nav.js
+++ public/components/nav/nav.js
@@ -20,10 +20,10 @@ const handleNavButton = (navId) => {
       document.getElementById("weather-now").style.display = "flex";
     }
     if (navId === "park-run-home") {
-      document.getElementById(navId).style.display = "block";
-      if (parkRuns.length > 0) return;      
+      if (parkRuns.length > 0) return;   
       computeParkRuns(responseRuns, computeFullParkRuns);      
       renderParkRunHome(parkRuns, fastestPR);
+      document.getElementById(navId).style.display = "block";
     }
     if (navId === "run-club-home"){
       fetchRunClub();
